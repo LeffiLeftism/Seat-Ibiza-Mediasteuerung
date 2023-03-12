e9c317a6374c0668e4b745d5d04a0b24b4af7114
LeffiLeftism <35105361+LeffiLeftism@users.noreply.github.com>
2023-03-12 14:26:58 +0100

    Version deployed to Ibiza

--- main/main.ino
+++ main/main.ino
@@ -1,20 +1,21 @@
 #include "voltage_transformer.h"
 #include "relais.h"
 #include "timer.h"
-//#define DEBUG
+#define DEBUG
 
 //Globale variables
 //Voltage Transformer
-VoltageTransformer IV(A0,1027, 469);          //Ignitionvoltate IV
-VoltageTransformer BV(A1,965, 469);          //Boardvoltage BV
-VoltageTransformer BAT2V(A6,1015, 463);       //Battery 2 voltate BAT2V
-VoltageTransformer CAPV(A7, 975, 472);       //Capacitorvoltate CAPV
+VoltageTransformer IV(A0,900, 469);          //Ignitionvoltate IV
+VoltageTransformer BV(A1,815, 459);          //Boardvoltage BV
+VoltageTransformer BAT2V(A6,900, 469);       //Battery 2 voltate BAT2V
+VoltageTransformer CAPV(A7, 850, 469);       //Capacitorvoltate CAPV
 //Relais
 Relais BAT2V_CAPV(2);                         //Connection Battery 2 to Capacitor
 Relais BV_BAT2V(3);                           //Connection Boardvoltage to Battery 2
 Relais BAT2V_PS(4);                           //Connection Battery 2 to PowerSupply
 Relais CAP(5);                                //Ref Voltage for AMP
 Relais AMP(6);                                //Ref Voltage for AMP
+Relais LoadLamp(7);                           //Lamp to load the CAP
 //Timer
 Timer T_BAT2(uint32_t(600000));               //Timer to load BAT2 within 10 Minutes
 Timer T_Ignition(uint32_t(20000));            //Timer to deactivate all after ignition is off
@@ -36,6 +37,7 @@ void setup() {
   BAT2V_PS.init();
   CAP.init();
   AMP.init();
+  LoadLamp.init();
   //Initialize timer
   T_BAT2.init();
   T_Ignition.init();
@@ -44,6 +46,8 @@ void setup() {
 void loop() {
   #ifdef DEBUG 
     Serial.println("------------------");
+    printVoltages();    
+    delay(100);
   #endif
   // put your main code here, to run repeatedly:
   T_BAT2.update();
@@ -53,37 +57,35 @@ void loop() {
 
 #ifdef DEBUG
   void printVoltages() {
-    Serial.print("PIN");
-    Serial.print(" | ");
-    Serial.print("R1  "); 
-    Serial.print(" | ");
-    Serial.print("R2  ");
-    Serial.print(" | ");
-    Serial.print("Read");
-    Serial.print(" | ");
-    Serial.print("Fac");
-    Serial.print(" | ");
-    Serial.print("Volt");
-    Serial.println();
-    IV.print();
-    BV.print();
+    //Serial.print("PIN");
+    //Serial.print(" | ");
+    //Serial.print("R1  "); 
+    //Serial.print(" | ");
+    //Serial.print("R2  ");
+    //Serial.print(" | ");
+    //Serial.print("Read");
+    //Serial.print(" | ");
+    //Serial.print("Fac");
+    //Serial.print(" | ");
+    //Serial.print("Volt");
+    //Serial.println();
     //uint16_t t1 = IV.getRaw();
     //uint16_t t2 = BV.getRaw();
-    delay(500);
-    //Serial.print(t1);
-    //Serial.print(" | ");
-    //Serial.print(t2);
-    //Serial.print(" | ");
-    //Serial.print(CAPV.getRaw());
-    //Serial.print(" | ");
-    //Serial.print(BAT2V.getRaw());
+    //delay(500);
+    Serial.print(IV.get());
+    Serial.print(" | ");
+    Serial.print(BV.get());
+    Serial.print(" | ");
+    Serial.print(BAT2V.get());
+    Serial.print(" | ");
+    Serial.print(CAPV.get());
     Serial.println();
     delay(3000);
   }
 #endif
 
 void checkIgnition() {
-  if(IV.get() > 11.5) { //Check if Ignition is turned on
+  if(IV.get() > 10.5) { //Check if Ignition is turned on
     checkup();
     T_Ignition.reset();
   } else if (!T_Ignition.isStartet()) {
@@ -104,7 +106,7 @@ void checkIgnition() {
 }
 
 void checkup(){
-  if ((BV.get() < 13.5)) { //Check if Engine is running -> voltage around 14.2V estimated
+  if ((BV.get() < 12.8)) { //Check if Engine is running -> voltage around 14.2V estimated
     // Do if engine is not running
     #ifdef DEBUG 
       Serial.println("BV not OK");
@@ -112,7 +114,7 @@ void checkup(){
     delay(50);
     goto reset;
   }
-  if ((BAT2V.get() < 11.5)) { //Check if BAT2 is loaded enought (min 11.5V)
+  if ((BAT2V.get() < 11)) { //Check if BAT2 is loaded enought (min 11.5V)
     // Do if BAT2 is too empty
     #ifdef DEBUG 
       Serial.println("BAT2V not OK");
@@ -145,15 +147,25 @@ void checkup(){
   BV_BAT2V.on();
   BAT2V_PS.on();
   CAP.on();
-  if ((CAPV.get() < 11.5)) { //Check if CAP is loaded enought (min 11.5V)
+  if ((CAPV.get() < 10.5)) { //Check if CAP is loaded enought (min 11.5V)
     // Do if CAP is too empty
     #ifdef DEBUG 
       Serial.println("CAPV not OK");
     #endif
     //ERROR somehow: sound? led?
     //Later: load CAP
-    delay(5000);
+    if (!LoadLamp.get()) {
+      #ifdef DEBUG
+        Serial.println("LoadLamp on");
+      #endif
+      LoadLamp.on();
+    }
     goto end;
+  } else if (LoadLamp.get()) {
+      #ifdef DEBUG
+        Serial.println("LoadLamp off");
+      #endif
+    LoadLamp.off();
   }
   BAT2V_CAPV.on();
   AMP.on();
@@ -174,9 +186,13 @@ void resetRelais() {
   #ifdef DEBUG 
     Serial.println("Reset Relais");
   #endif
+  LoadLamp.off();  
   AMP.off();  
   CAP.off();
   BAT2V_CAPV.off();
   BV_BAT2V.off();
   BAT2V_PS.off();
-  //The system should shutdown if ignition is off, because no power}
+  //The system should shutdown if ignition is off, because no power
+
+}
+
